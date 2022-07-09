c5659c6be143f90a573092307875bdfd4dab58f9
Rob Arthur <rarthur@lightkeeper.com>
2022-07-09 14:49:06 -0400

    Check we can test with env variables

--- python/src/main.py
+++ python/src/main.py
@@ -1,7 +1,13 @@
+import os
 import requests
 
 def lambda_handler(event, context):
-    message = 'Hello {} {}!'.format(event['first_name'], event['last_name'])  
+    API_KEY = os.getenv('API_KEY')
+
+    if not API_KEY:
+        raise ValueError('API_KEY environment variable is not set')
+
+    message = f'API_KEY: {API_KEY}'  
     return { 
         'message' : message
     }
--- python/test/run.sh
+++ python/test/run.sh
@@ -3,4 +3,4 @@
 # set workingdir to script location
 cd "${0%/*}"
 
-python-lambda-local -l ../../env/lib/ -f lambda_handler -t 5 ../src/main.py event.json
+python-lambda-local -l ../../env/lib/ -f lambda_handler -t 5 -e ../env_variables.json ../src/main.py event.json
