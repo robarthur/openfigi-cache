17c3c3bb361af98f4a376ca7b712fe292c96f3e7
Rob Arthur <rarthur@lightkeeper.com>
2022-07-09 15:11:11 -0400

    Example end to end lambda test retrieving tickers

--- python/src/main.py
+++ python/src/main.py
@@ -7,7 +7,12 @@ def lambda_handler(event, context):
     if not API_KEY:
         raise ValueError('API_KEY environment variable is not set')
 
-    message = f'API_KEY: {API_KEY}'  
+    MAPPING_V3_API='https://api.openfigi.com/v3/mapping'
+    headers={}
+    headers.update({'X-OPENFIGI-KEY': API_KEY})
+
+    response = requests.post(MAPPING_V3_API, headers=headers, json=event)
+
     return { 
-        'message' : message
+        'response' : response.json()
     }
