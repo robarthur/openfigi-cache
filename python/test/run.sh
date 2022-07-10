e9e4e701ff24b7afc04ec86ea53164e942058717
Rob Arthur <rarthur@lightkeeper.com>
2022-07-10 15:22:54 -0400

    Test dynamo locally with docker

--- python/test/run.sh
+++ python/test/run.sh
@@ -3,4 +3,17 @@
 # set workingdir to script location
 cd "${0%/*}"
 
+docker-compose -f ../../docker/docker-compose.yml up --force-recreate -d
+
+aws dynamodb create-table \
+    --table-name MappingsV3 \
+    --attribute-definitions \
+        AttributeName=id,AttributeType=S \
+        AttributeName=figi,AttributeType=S \
+    --key-schema \
+        AttributeName=id,KeyType=HASH \
+        AttributeName=figi,KeyType=RANGE \
+    --billing-mode PAY_PER_REQUEST \
+    --endpoint-url http://localhost:8000
+
 python-lambda-local -l ../../env/lib/ -f lambda_handler -t 5 -e ../env_variables.json ../src/main.py event.json
