1a3d827608a30c6db57f1808a19af9b2365c382f
Rob Arthur <rarthur@lightkeeper.com>
2022-07-16 11:50:21 -0400

    Fix parameter group name

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "aws_elasticache_cluster" "example" {
   engine               = "redis"
   node_type            = "cache.t4g.micro"
   num_cache_nodes      = 1
-  parameter_group_name = "default.redis6.2"
+  parameter_group_name = "default.redis6.x"
   engine_version       = "6.2"
   port                 = 6379
 }
