56036f7b2eaaf7b051b059bbf5bd031ca065d4b7
Rob Arthur <rarthur@lightkeeper.com>
2022-07-16 10:56:13 -0400

    Move terraform to root of project

---
+++ main.tf
@@ -0,0 +1,96 @@
+# Require TF version to be same as or greater than 0.12.13
+terraform {
+  required_version = ">=1.2.5"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.22.0"
+    }
+  }
+  # For now lets just use local state
+  #backend "s3" {
+  #  bucket         = "bucket"
+  #  key            = "terraform.tfstate"
+  #  region         = "us-east-1"
+  #  dynamodb_table = "aws-locks"
+  #  encrypt        = true
+  #}
+}
+
+# Download any stable version in AWS provider of 4.22.0 or higher in 4.22.0 train
+provider "aws" {
+  region  = "us-east-1"
+}
+
+#
+# Elasticache Resources
+#
+
+# resource "aws_elasticache_cluster" "example" {
+#   cluster_id           = "openfigi-cache"
+#   engine               = "redis"
+#   node_type            = "cache.t4g.micro"
+#   num_cache_nodes      = 1
+#   parameter_group_name = "default.redis6.2"
+#   engine_version       = "6.2"
+#   port                 = 6379
+# }
+
+#
+# Lambda Resources
+#
+
+resource "null_resource" "package_lambda" {
+  provisioner "local-exec" {
+    command = "pip install -r ./requirements.txt -t deploy/source/ --upgrade"
+  }
+
+  provisioner "local-exec" {
+    command = "cp python/src/main.py deploy/main.py"
+  }
+
+  triggers = {
+    dependencies_versions = filemd5("./requirements.txt"),
+    source_version  = filemd5("python/src/main.py"),
+  }
+}
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_dir = "deploy/"
+  output_path = "deploy/open_figi_cache.zip"
+}
+
+resource "aws_s3_bucket" "lambda_deploy" {
+  bucket_prefix = "lambda_deploy"
+}
+
+resource "aws_s3_bucket_acl" "lambda_deploy" {
+  bucket = aws_s3_bucket.lambda_deploy.id
+  acl    = "private"
+}
+
+resource "aws_lambda_function" "open_figi_cache" {
+  function_name = "OpenFigiCache"
+
+  # The bucket name as created earlier with "aws s3api create-bucket"
+  s3_bucket = aws_s3_bucket.lambda_deploy.id
+  s3_key    = data.archive_file.lambda.output_path
+
+  # "main" is the filename within the zip file (main.js) and "handler"
+  # is the name of the property under which the handler function was
+  # exported in that file.
+  handler = "main.lambda_handler"
+  runtime = "python3.9"
+
+  role = "${aws_iam_role.lambda_exec.arn}"
+}
+
+
+#
+# API Gateway Resources
+#
+
+#
+# IAM Resources 
+#
