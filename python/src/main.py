72f695e5ebd0ba28144e7bb410c59e8b1ed9b660
Rob Arthur <rarthur@lightkeeper.com>
2022-07-18 21:31:42 -0400

    black format

--- python/src/main.py
+++ python/src/main.py
@@ -4,26 +4,33 @@ import boto3
 import requests
 import redis
 
+
 def lambda_handler(event, context):
-    API_KEY = os.getenv('API_KEY')
-    REDIS_ENDPOINT = os.getenv('REDIS_ENDPOINT') or "localhost"
-    REDIS_PORT =  os.getenv('REDIS_PORT') or "6379"
-    MAPPING_V3_API='https://api.openfigi.com/v3/mapping'
+    API_KEY = os.getenv("API_KEY")
+    REDIS_ENDPOINT = os.getenv("REDIS_ENDPOINT") or "localhost"
+    REDIS_PORT = os.getenv("REDIS_PORT") or "6379"
+    MAPPING_V3_API = "https://api.openfigi.com/v3/mapping"
     USE_SSL = False
 
-    body = json.loads(event.get('body'))
+    body = json.loads(event.get("body"))
 
     if not API_KEY:
-        raise ValueError('API_KEY environment variable is not set')
+        raise ValueError("API_KEY environment variable is not set")
 
     print(f"{REDIS_ENDPOINT=}, {REDIS_PORT=}, {USE_SSL=}")
     print(f"{body=}")
-    r = redis.Redis(host=REDIS_ENDPOINT, port=REDIS_PORT, charset="utf-8", decode_responses=True, socket_connect_timeout=2)
+    r = redis.Redis(
+        host=REDIS_ENDPOINT,
+        port=REDIS_PORT,
+        charset="utf-8",
+        decode_responses=True,
+        socket_connect_timeout=2,
+    )
     keys = get_cache_keys_from_body(body)
 
     # Get as much as we can from cache
-    #cache_result = [json.loads(k) for k in r.mget(keys) if k ]
-    cache_result = [ json.loads(k) if k else None for k in r.mget(keys) ]
+    # cache_result = [json.loads(k) for k in r.mget(keys) if k ]
+    cache_result = [json.loads(k) if k else None for k in r.mget(keys)]
 
     print(f"{cache_result =}")
     cache_misses = get_cache_misses(cache_result, body)
@@ -32,8 +39,8 @@ def lambda_handler(event, context):
     response_json = []
     # Get everything else from API
     if cache_misses:
-        headers={}
-        headers.update({'X-OPENFIGI-KEY': API_KEY})
+        headers = {}
+        headers.update({"X-OPENFIGI-KEY": API_KEY})
         print(f"Making request to open figi API: {headers=}, {cache_misses=}")
         response = requests.post(MAPPING_V3_API, headers=headers, json=cache_misses)
         print(f"response code={response.status_code}")
@@ -47,18 +54,18 @@ def lambda_handler(event, context):
     # Concatentate the results
     cache_result.extend(response_json)
 
-    return {
-    "isBase64Encoded": False,
-    "statusCode": 200,
-    "body": cache_result
-    }      
+    return {"isBase64Encoded": False, "statusCode": 200, "body": cache_result}
+
 
 def get_cache_misses(cache_result, body):
     cache_misses = []
     if None in cache_result:
-        empty_cache_result_indexes = [i for i, x in enumerate(cache_result) if x==None]
+        empty_cache_result_indexes = [
+            i for i, x in enumerate(cache_result) if x == None
+        ]
         cache_misses = [body[i] for i in empty_cache_result_indexes]
     return cache_misses
 
+
 def get_cache_keys_from_body(body):
-    return [ f"{d['idType']}_{d['idValue']}" for d in body ]
+    return [f"{d['idType']}_{d['idValue']}" for d in body]
